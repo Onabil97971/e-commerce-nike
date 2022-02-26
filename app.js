5e70a8dbad61d89ff9d6e8d852a110a62b53c62b
Omar Nabil Mohamed Ahmed Omar <Onabil97971@yahoo.com>
2022-02-26 05:46:40 +0200

    commit

--- app.js
+++ app.js
@@ -79,10 +79,49 @@ const products = [
     },
   ];
 
+
+  let choosenProduct = products[0]
+
+
+  const currentProductImg = document.querySelector(".productImg");
+  const currentProductTitle = document.querySelector(".productTitle");
+  const currentProductPrice = document.querySelector(".productPrice");
+  const currentProductColors = document.querySelectorAll(".color");
+  const currentProductSizes = document.querySelectorAll(".size");
   
 
 menuItems.forEach((item, index) => {
     item.addEventListener("click", () => {
+        //change the current slide
         wrapper.style.transform = `translateX(${-100 * index}vw)`;
+
+        //change the choosen product 
+        choosenProduct = products[index]
+
+        //change the text of current product 
+        currentProductTitle.textContent = choosenProduct.title
+        currentProductPrice.textContent = "$" + choosenProduct.price 
+        currentProductImg.src = choosenProduct.colors[0].img
+        // assigning new colors 
+        currentProductColors.forEach((color,index)=> {
+            color.style.backgroundColor = choosenProduct.colors[index].code;
+        });
     });
+});
+
+currentProductColors.forEach((color,index)=> {
+  color.addEventListener("click", ()=>{
+    currentProductImg.src = choosenProduct.colors[index].img
+  });
+});
+
+currentProductSizes.forEach((size,index)=>{
+  size.addEventListener("click",()=>{
+      currentProductSizes.forEach(size=> {
+        size.style.backgroundColor = "white"
+        size.style.color = "black"
+      });
+      size.style.backgroundColor = "black"
+      size.style.color = "white"
+  }); 
 });
